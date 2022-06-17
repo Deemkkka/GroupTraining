43188372b7831d1cb3a4acbf59898f56f0e0333f
Deem <Deem@DEEM>
2022-06-17 16:39:27 +0300

    реализована группировка

--- GroupTraining/MainWindow.xaml.cs
+++ GroupTraining/MainWindow.xaml.cs
@@ -20,9 +20,42 @@ namespace GroupTraining
     /// </summary>
     public partial class MainWindow : Window
     {
+        User[] users = new User[]
+        {
+            new User("Petr", "Russia", 20),
+            new User("Petr1", "America", 21),
+            new User("Petr2", "Russia", 21),
+            new User("Petr3", "America", 20),
+            new User("Petr4", "Russia", 20),
+            new User("Petr5", "Russia", 25),
+            new User("Petr6", "Brazil", 25),
+            new User("Petr7", "Brazil", 20)
+        };
+
         public MainWindow()
         {
             InitializeComponent();
+
+            ListViewDataUsers.ItemsSource = users;
+            ComboBoxGroup.ItemsSource = new string[] { "None", "Country", "Age" };
+        }
+
+        public void GroupList()
+        {
+            ListViewDataUsers.Items.GroupDescriptions.Clear();
+            var property = ComboBoxGroup.SelectedItem as string;
+
+            if (property == "None")
+            {
+                return;
+            }
+
+            ListViewDataUsers.Items.GroupDescriptions.Add(new PropertyGroupDescription(property));
+        }
+
+        private void ComboBoxGroup_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            GroupList();
         }
     }
 }
