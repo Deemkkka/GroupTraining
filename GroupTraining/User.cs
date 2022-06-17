b9992b285e5067811e12573ee4cd4aee4c46d498
Deem <Deem@DEEM>
2022-06-17 14:28:02 +0300

    Добавьте файлы проекта.

---
+++ GroupTraining/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroupTraining
+{
+    internal class User
+    {
+        public string Name { get; set; }
+        public string Country { get; set; }
+        public int Age { get; set; }
+
+        public User(string name, string country, int age)
+        {
+            Name = name;
+            Country = country;
+            Age = age;
+        }
+    }
+}
