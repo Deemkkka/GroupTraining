bac9bd6adf5aad78e44396aa86113e1ece8de3f4
Deem <Deem@DEEM>
2022-06-17 17:51:27 +0300

    добавлен базовый класс Command, добавлена LambdaCommand, добавлен команда закрытия приложения

---
+++ GroupTraining/Infrastructure/Commands/ApplicationCloseCommand.cs
@@ -0,0 +1,16 @@
+﻿using GroupTraining.Infrastructure.Commands.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace GroupTraining.Infrastructure.Commands
+{
+    internal class ApplicationCloseCommand : Command
+    {
+        public override bool CanExecute(object? parameter) => true;
+        public override void Execute(object? parameter) => Application.Current.Shutdown();
+    }
+}
